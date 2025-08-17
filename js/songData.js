da7ca0837fb2648b5a6081b77902e88724189afa
rishisoyal <rishisoyal510@gmail.com>
2025-08-17 17:31:16 +0530

    update song list

--- js/songData.js
+++ js/songData.js
@@ -1,15 +1,4 @@
-const playlists = {
-    Favorite: [
-        { poster: "", src: ".\\songs\\Fav\\SPOT theme but it's PHONK.mp3" },
-        { poster: "", src: ".\\songs\\Fav\\Chime - Micrology.mp3" },
-        { poster: "", src: ".\\songs\\Fav\\Cyberpunk 2077 music.mp3" },
-        { poster: "", src: ".\\songs\\Fav\\Dead Silence Theme (Slicey Remix).mp3" },
-        { poster: "", src: ".\\songs\\Fav\\Electro Theme (The Amazing Spider-Man 2) on Guitar.mp3" },
-        { poster: "", src: ".\\songs\\Fav\\Figaro (Dubstep Remix).mp3" },
-        { poster: "", src: ".\\songs\\Fav\\MARIACHI FUNK.mp3" },
-        { poster: "", src: ".\\songs\\Fav\\THE BANJO BEAT - RICKY DESKTOP.mp3" },
-        { poster: "", src: ".\\songs\\Fav\\TIN - Ticking.mp3" }
-    ],
+const playlists = {    
     "Alan Walker": [
         { poster: ".\\assets\\posters\\alan_walker_dark_side.jpg", src: ".\\songs\\Alan Walker\\Alan Walker - Darkside (feat. AuRa and Tomine Harket).mp3" },
         { poster: ".\\assets\\posters\\alan_walker_don't_you_hold_me_down.jpg", src: ".\\songs\\Alan Walker\\Alan Walker & Georgia Ku - Don't You Hold Me Down (Official Music Video).mp3" },
